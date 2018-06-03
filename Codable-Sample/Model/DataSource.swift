e42e1734f731e960a7fb01b3e5940c13d3882c15
ddd503 <skbsporta7@gmail.com>
2018-06-03 19:59:56 +0900

    ローカルからjsonデータを取得してマッピングして返すDAOを作成

---
+++ Codable-Sample/Model/DataSource.swift
@@ -0,0 +1,54 @@
+//
+//  DataSource.swift
+//  Codable-Sample
+//
+//  Created by kawaharadai on 2018/06/03.
+//  Copyright © 2018年 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+final class DataSource: NSObject {
+    
+    /// ローカルのjsonファイルからユーザーデータを取得してマッピングしてVCに返す
+    private func create() -> UserDataResponse? {
+        
+        guard let filePath = Bundle.main.path(forResource: "UserData-Sample", ofType: "json") else {
+            fatalError("json is not found in main Bundle.")
+        }
+        
+        do {
+            let jsonData = try Data(contentsOf: URL(fileURLWithPath: filePath))
+            let decoder = JSONDecoder()
+            let responseData = try decoder.decode(UserDataResponse.self, from: jsonData)
+            
+            return responseData
+            
+        } catch let error {
+            print("error: \(error.localizedDescription)")
+            return nil
+        }
+        
+    }
+    
+    /// 配列スタートの場合
+    private func create() -> [UserDataResponseArray]? {
+        
+        guard let filePath = Bundle.main.path(forResource: "UserData-Sample", ofType: "json") else {
+            fatalError("json is not found in main Bundle.")
+        }
+        
+        do {
+            let jsonData = try Data(contentsOf: URL(fileURLWithPath: filePath))
+            let decoder = JSONDecoder()
+            let responseData = try decoder.decode([UserDataResponseArray].self, from: jsonData)
+            
+            return responseData
+            
+        } catch let error {
+            print("error: \(error.localizedDescription)")
+            return nil
+        }
+        
+    }
+}
